2831a7f36fa50f3821885a78dd12c340449c2a39
wilcockj <jameswilcock8@gmail.com>
2020-07-14 00:16:36 -0700

    added save

--- Cookie/ocr.py
+++ Cookie/ocr.py
@@ -1,8 +1,7 @@
 import cv2
 import pyautogui
 import time
-im = pyautogui.screenshot(region=(3214,551, 3262,588))
-location = pyautogui.locateOnScreen(im)
+location = pyautogui.locateOnScreen("images/cookie.png")
 while True:
     time.sleep(2)
     print(location)
